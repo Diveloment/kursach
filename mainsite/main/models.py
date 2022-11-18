2669f70582879d4c7808c6eb9f36ebaad996566a
u413 <a@a.ru>
2022-11-18 14:00:38 +0300

    "db fixed"

--- mainsite/main/models.py
+++ mainsite/main/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -12,5 +14,5 @@ class User(AbstractUser):
 
     email_verify = models.BooleanField(default=False)
     user_role = models.CharField(default='клиент', max_length=55)
-    phone = models.CharField(default='', max_length=12)
-    address = models.CharField(default='', max_length=254)
+    phone = models.CharField(default='', max_length=12, unique=False, blank=True)
+    address = models.CharField(default='', max_length=254, unique=False, blank=True)
