3cc317911661073fb56682ed068d697f0e701adc
Feodor <a2@a2.ru>
2022-12-05 04:13:20 +0300

    request basic functions complete

--- mainsite/main/models.py
+++ mainsite/main/models.py
@@ -33,8 +33,13 @@ class Request(models.Model):
         ('awaiting', 'ожидание')
     )
 
-    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=25)
-    content = models.TextField(blank=True)
-    status = models.CharField(max_length=15, choices=REQ_STATUS, default='awaiting')
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='createdBy', verbose_name='заявитель')
+    title = models.CharField(max_length=25, verbose_name='название')
+    content = models.TextField(blank=True, verbose_name='содержание')
+    status = models.CharField(max_length=15, choices=REQ_STATUS, default='awaiting', verbose_name='статус')
+    leads = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='leads', null=True, blank=True, verbose_name='куратор')
+
+    class Meta:
+        verbose_name = _("заявка")
+        verbose_name_plural = _("заявки")
 
